dc9a2f2441d3a4f967babda4e9c0325a4b9c70c0
Sunnat <s.erkinov.77@gmail.com>
2021-04-26 17:46:56 +0300

    Theme7

--- Assets/Scripts/TransitionElement.cs
+++ Assets/Scripts/TransitionElement.cs
@@ -1,90 +1,90 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class TransitionElement : MonoBehaviour
-{
+//public class TransitionElement : MonoBehaviour
+//{
 
-    [HideInInspector] public float startAtSeconds = 0f;
-    [HideInInspector] public bool isStartFromThisObj = false;
-    /*[HideInInspector]*/ public TransitionElement lastObject = null;
-    [HideInInspector] public float timeOfRange = 0.5f;
-    [HideInInspector] public string[] TypeOfTransition = new string[] { "Lerp", "Move Toward", "Scale" };
-    [HideInInspector] public int IndexOfTypes = 0;
-    [HideInInspector] public Vector3 ToThatLocation;
-    [HideInInspector] public float ScaleRadius = 0.1f;
+//    [HideInInspector] public float startAtSeconds = 0f;
+//    [HideInInspector] public bool isStartFromThisObj = false;
+//    /*[HideInInspector]*/ public TransitionElement lastObject = null;
+//    [HideInInspector] public float timeOfRange = 0.5f;
+//    [HideInInspector] public string[] TypeOfTransition = new string[] { "Lerp", "Move Toward", "Scale" };
+//    [HideInInspector] public int IndexOfTypes = 0;
+//    [HideInInspector] public Vector3 ToThatLocation;
+//    [HideInInspector] public float ScaleRadius = 0.1f;
 
-    public delegate void StartTransitionBoolChanged();
-    public event StartTransitionBoolChanged ToNextObject;
+//    public delegate void StartTransitionBoolChanged();
+//    public event StartTransitionBoolChanged ToNextObject;
 
-    [HideInInspector] public bool isGoNextObject;
-    public bool IsGoNextObject
-    {
-        get
-        {
-            return isGoNextObject;
-        }
-        set
-        {
-            isGoNextObject = value;
-            if (ToNextObject != null)
-            {
-                ToNextObject();
-            }
-        }
-    }
+//    [HideInInspector] public bool isGoNextObject;
+//    public bool IsGoNextObject
+//    {
+//        get
+//        {
+//            return isGoNextObject;
+//        }
+//        set
+//        {
+//            isGoNextObject = value;
+//            if (ToNextObject != null)
+//            {
+//                ToNextObject();
+//            }
+//        }
+//    }
 
-    private bool isStartTransition;
-    public bool IsStartTransition
-    {
-        get { return isStartTransition; }
-        set
-        {
-            if (value == isStartTransition)
-                return;
+//    private bool isStartTransition;
+//    public bool IsStartTransition
+//    {
+//        get { return isStartTransition; }
+//        set
+//        {
+//            if (value == isStartTransition)
+//                return;
 
-            isStartTransition = value;
-            if (isStartTransition)
-            {
-                StartAnim();
-            }
-        }
-    }
+//            isStartTransition = value;
+//            if (isStartTransition)
+//            {
+//                StartAnim();
+//            }
+//        }
+//    }
 
-    private void StartAnim()
-    {
-        StartCoroutine(startNextObjAnim());
-    }
+//    private void StartAnim()
+//    {
+//        StartCoroutine(startNextObjAnim());
+//    }
 
-    private IEnumerator startNextObjAnim()
-    {
-        yield return new WaitForSeconds(timeOfRange);
-        isGoNextObject = true;
-    }
+//    private IEnumerator startNextObjAnim()
+//    {
+//        yield return new WaitForSeconds(timeOfRange);
+//        isGoNextObject = true;
+//    }
 
-    private void Start()
-    {
-        IsStartTransition = false;
+//    private void Start()
+//    {
+//        IsStartTransition = false;
 
-        if (isStartFromThisObj)
-            StartCoroutine(startAt());
-        else
-            lastObject.ToNextObject += LastObject_ToNextObject;
-    }
+//        if (isStartFromThisObj)
+//            StartCoroutine(startAt());
+//        else
+//            lastObject.ToNextObject += LastObject_ToNextObject;
+//    }
 
-    private void LastObject_ToNextObject()
-    {
-        IsStartTransition = true;
-    }
+//    private void LastObject_ToNextObject()
+//    {
+//        IsStartTransition = true;
+//    }
 
-    private void FixedUpdate()
-    {
+//    private void FixedUpdate()
+//    {
 
-    }
+//    }
 
-    private IEnumerator startAt()
-    {
-        yield return new WaitForSeconds(startAtSeconds);
-        IsStartTransition = true;
-    }
-}
+//    private IEnumerator startAt()
+//    {
+//        yield return new WaitForSeconds(startAtSeconds);
+//        IsStartTransition = true;
+//    }
+//}
