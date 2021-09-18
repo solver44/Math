d74188a7aa71232430c685efbe031f9dee698baa
Sunnat <s.erkinov.77@gmail.com>
2021-09-18 17:00:10 +0500

    st

---
+++ Assets/Scripts/MathGames/Fourth/ClickButton4.cs
@@ -0,0 +1,265 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton4 : MonoBehaviour
+{
+    [HideInInspector] public bool StartWithoutPlayer = true;
+    [HideInInspector] public GameObject[] Lvls = null;
+    [HideInInspector] public GameObject[] Questions = null;
+    [HideInInspector] public GameObject[] AnswerBtns = null;
+    [HideInInspector] public Sprite[] Elements = null;
+    [HideInInspector] public Text Stats;
+
+    [HideInInspector] public bool Finish = false;
+
+    private Animator symbolAnimController => GameObject.FindGameObjectWithTag("Anim").GetComponent<Animator>();
+
+    private void lose()
+    {
+        Debug.Log("Finish");
+        if (!StartWithoutPlayer)
+        {
+            try
+            {
+                PunView.RPC("CheckWinOrLose", RpcTarget.All, results);
+            }
+            catch {
+                CreatorMapSecond map = GameObject.Find("Creator").GetComponent<CreatorMapSecond>();
+                map.WinOrLose(false);
+            }
+        }
+        else
+        {
+            CreatorMapSecond map = GameObject.Find("Creator").GetComponent<CreatorMapSecond>();
+            map.WinOrLose(false);
+        }
+    }
+    private void changeStats()
+    {
+        int i = Int32.Parse(Stats.text) + 1;
+        Stats.text = i.ToString();
+    }
+    int[] results;
+    ScaleEffect effect = new ScaleEffect();
+    void increaseSizeUI()
+    {
+        //StartCoroutine(MoveSmooth(rect, new Vector2(rect.localPosition.x, rect.localPosition.y + interval)));
+        ScrollRect lineScrollRect = LBScrollContent.GetComponent<ScrollRect>();
+        //StartCoroutine(MoveSmooth(scrollRect, new Vector2(scrollRect.preferredHeight)));
+        StartCoroutine(LerpToChild(lineScrollRect, Lvls[currentIndex].GetComponent<RectTransform>(), false));
+    }
+    private IEnumerator LerpToChild(ScrollRect _scrollRectComponent, RectTransform target, bool isMain)
+    {
+        //StartCoroutine(LerpToChild(lineScrollRect, Lines[_currentIndex].GetComponent<RectTransform>(), false));
+        RectTransform child;
+        if (isMain)
+            child = _scrollRectComponent.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<RectTransform>();
+        else
+            child = _scrollRectComponent.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<RectTransform>();
+
+        Vector2 _lerpTo = child.anchoredPosition - (Vector2)_scrollRectComponent.transform.InverseTransformPoint(target.position) - new Vector2(0, 60);
+
+        Canvas.ForceUpdateCanvases();
+
+        float decelerate = 1f;
+        for (float i = 0; i < 1; i += Time.deltaTime * decelerate)
+        {
+            child.anchoredPosition = Vector2.Lerp(child.anchoredPosition, _lerpTo, i);
+            if (Vector2.Distance(child.anchoredPosition, _lerpTo) < 0.25f)
+            {
+                child.anchoredPosition = _lerpTo;
+                yield break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+    }
+
+    int makeRandomlyNumWithoutEquals(int[] targetNum, int min, int max)
+    {
+        int num;
+        do
+        {
+            num = UnityEngine.Random.Range(min, max);
+        }
+        while (targetNum.Contains(num));
+
+        return num;
+    }
+
+    ScaleEffect scl = new ScaleEffect();
+    bool wait = false;
+
+    [HideInInspector] public PhotonView PunView = null;
+
+
+    GameObject LBScrollContent;
+    private IEnumerator waitSeconds()
+    {
+        wait = true;
+        yield return new WaitForSeconds(.8f);
+        wait = false;
+    }
+    int currentIndex = 0;
+    private IEnumerator IeStart()
+    {
+        yield return new WaitForSeconds(0.4f);
+        StartCoroutine(effect.MoveAnimTowards(Questions[currentIndex].transform, new Vector2(Questions[currentIndex].transform.localPosition.x, 0), true, 8f));
+    }
+    bool won()
+    {
+        if (currentIndex >= Lvls.Length)
+        {
+            Finish = true;
+            Debug.Log("Finish");
+            if (!StartWithoutPlayer) {
+                try
+                {
+                    PunView.RPC("CheckWinOrLose", RpcTarget.All, results);
+                }
+                catch {
+                    CreatorMapThird map = GameObject.Find("Creator").GetComponent<CreatorMapThird>();
+                    map.WinOrLose(true);
+                }
+            }
+            else
+            {
+                CreatorMapThird map = GameObject.Find("Creator").GetComponent<CreatorMapThird>();
+                map.WinOrLose(true);
+            }
+            return true;
+        }
+        return false;
+    }
+    public void CreateNewAns()
+    {
+        StartCoroutine(IeStart());
+
+        increaseSizeUI();
+
+        Lvls[currentIndex].transform.GetChild(0).gameObject.SetActive(true);
+        RectTransform num = Lvls[currentIndex].transform.GetChild(1).GetComponent<RectTransform>();
+        num.offsetMin = new Vector2(60, num.offsetMin.y);
+    }
+    public void CheckEqual(bool pressedEqual)
+    {
+        if (Finish || wait)
+            return;
+
+        bool isEqual = false;
+
+        List<Image> firstVals = new List<Image>(); 
+        List<Image> secondVals = new List<Image>();
+
+        for (int i = 0; i < Questions[currentIndex].transform.childCount; i++)
+        {
+            for (int l = 0; l < Questions[currentIndex].transform.GetChild(i).transform.childCount; l++)
+            {
+                foreach (var temp in Questions[currentIndex].transform.GetChild(i).transform.GetChild(l).GetComponentsInChildren<Image>())
+                {
+                    if (i == 0)
+                    {
+                        firstVals.Add(temp);
+                    }
+                    else
+                    {
+                        secondVals.Add(temp);
+                    }
+                }
+            }
+        }
+        if (firstVals.Select(c => c.overrideSprite).All(secondVals.Select(c => c.overrideSprite).Contains) && firstVals.Select(c => c.color).All(secondVals.Select(c => c.color).Contains))
+        {
+            isEqual = true;
+        }
+        if (isEqual == pressedEqual)
+        {
+            changeStats();
+
+            symbolAnimController.SetLayerWeight(3, 0f);
+            symbolAnimController.SetLayerWeight(1, 1f);
+            symbolAnimController.SetTrigger("start");
+        }
+        else
+        {
+            symbolAnimController.SetLayerWeight(1, 0f);
+            symbolAnimController.SetLayerWeight(3, 1f);
+            symbolAnimController.SetTrigger("start");  
+        }
+
+        StartCoroutine(waitAnim(isEqual, pressedEqual));
+    }
+
+    private IEnumerator waitAnim(bool isEqual, bool pressedEqual)
+    {
+        yield return new WaitForSeconds(1f);
+
+        setInactivePrevious(isEqual == pressedEqual);
+        currentIndex++;
+
+        if (currentIndex >= Questions.Length)
+        {
+            won();
+            yield break;
+        }
+
+        CreateNewAns();
+
+        StartCoroutine(waitSeconds());
+    }
+    private void setInactivePrevious(bool isAnsTrue)
+    {
+        StartCoroutine(effect.MoveAnimTowards(Questions[currentIndex].transform, new Vector2(Questions[currentIndex].transform.localPosition.y, -500), true, 8f));
+
+        if (isAnsTrue)
+        {
+            Lvls[currentIndex].GetComponent<Image>().color = new Color32(15, 225, 10, 200);
+        }
+        else
+        {
+            Lvls[currentIndex].GetComponent<Image>().color = new Color32(225, 15, 10, 200);
+        }
+        Lvls[currentIndex].transform.GetChild(0).gameObject.SetActive(false);
+        Lvls[currentIndex].transform.GetChild(1).GetComponent<RectTransform>().offsetMin = new Vector2(0, Lvls[currentIndex].transform.GetChild(1).GetComponent<RectTransform>().offsetMin.y);
+    }
+
+    private int[] getRandomNumber(int min, int max, int count, bool equalNums)
+    {
+        int rand = UnityEngine.Random.Range(min, max);
+        int[] rands = new int[count];
+        for (int i = 0; i < rands.Length; i++)
+        {
+            rands[i] = -1;
+        }
+        for (int i = 0; i < rands.Length; i++)
+        {
+            while (!equalNums && rands.Contains(rand))
+            {
+                rand = UnityEngine.Random.Range(min, max);
+            }
+            if (equalNums)
+                rand = UnityEngine.Random.Range(min, max);
+
+            rands[i] = rand;
+        }
+
+        return rands;
+    }
+    void SetPunView()
+    {
+        if (!StartWithoutPlayer)
+            PunView = GameObject.Find("enemy").transform.GetComponent<PhotonView>();
+    }
+    private void Start()
+    {
+        results = new int[Lvls.Length];
+
+        LBScrollContent = GameObject.FindGameObjectWithTag("Levels");
+        symbolAnimController.SetLayerWeight(0, 0f);
+    }
+}
---
+++ Assets/Scripts/MathGames/Fourth/CreatorMapFourth.cs
@@ -0,0 +1,216 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ExitGames.Client.Photon;
+using System;
+using UnityEngine.UI;
+using System.Linq;
+
+public class CreatorMapFourth : MonoBehaviourPunCallbacks, IOnEventCallback
+{
+    public bool StartWithoutPlayer = false;
+    [Header("Main")]
+    public GameObject[] QuestionTemplate = null;
+    public GameObject[] AnswerButtons = null;
+    private GameObject[] questions = null;
+    public Transform Parent = null;
+    public int CountQuestions = 10;
+    public ClickButton4 click = null;
+    [Header("Elements")]
+    public Sprite[] Elements = null;
+    [Header("LeftBar")]
+    public GameObject PlayerLvlTemplate = null;
+    public GameObject LeftBar = null;
+    private GameObject[] lvls = null;
+    private GameObject _scrollContent = null;
+    [Header("RightBar")]
+    public GameObject EnemyLvlTemplate = null;
+    public GameObject RightBar = null;
+    [Header("End")]
+    public GameObject WaitingPanel = null;
+    public GameObject WinPanel = null;
+    public GameObject ImageWinPanel = null;
+    public Text TextWinPanel = null;
+    [Header("Info")]
+    public GameObject Player = null;
+    public GameObject Enemy = null;
+
+    GameObject currentTemp;
+    Image[] currentTempChildren = new Image[3];
+    List<Image[]> columnTempChildren = new List<Image[]>();
+    private void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+        _scrollContent = LeftBar.transform.GetChild(0).transform.GetChild(0).gameObject;
+        setInfos();
+
+        click.AnswerBtns = AnswerButtons;
+        click.Elements = Elements;
+
+        questions = new GameObject[CountQuestions];
+    }
+    private void setInfos()
+    {
+        Player.transform.GetChild(1).GetComponentInChildren<Text>().text = PlayerPrefs.GetString("nameUser") + " " + PlayerPrefs.GetString("surnameUser");
+        Text tempUser = Player.transform.GetChild(0).transform.GetChild(1).transform.GetChild(0).GetComponent<Text>();
+        if(PlayerPrefs.HasKey("CurrentLevel"))
+            tempUser.text = tempUser.text = PlayerPrefs.GetInt("CurrentLevel").ToString();
+        else
+            tempUser.text = tempUser.text = "0";
+    }
+
+    int cntElementsCount;
+    void setQuestions()
+    {
+        questions = new GameObject[CountQuestions];
+
+        cntElementsCount = QuestionTemplate[0].transform.GetChild(0).transform.childCount;
+        for (int i = 0; i < CountQuestions; i++)
+        {
+            questions[i] = Instantiate(QuestionTemplate[0], Parent.transform);
+            questions[i].name = "Q" + (i + 1).ToString();
+
+            makeQuestion(i, UnityEngine.Random.Range(0, 2) == 1);
+            
+            questions[i].transform.localPosition = new Vector2(0, 500);
+        }
+        StartCoroutine(IStart());
+    }
+
+    GameObject tempChild;
+    private void makeQuestion(int i, bool isElementsEqual)
+    {
+       
+    }
+    private IEnumerator IStart()
+    {
+        yield return new WaitForSeconds(1);
+        StartCoroutine(effect.MoveAnimTowards(questions[0].transform, new Vector2(0, 0), true, 8f));
+    }
+    private void Start()
+    {
+        setLvlPanels();
+        setQuestions();
+
+        click.Lvls = lvls;
+        click.Questions = questions;
+        click.Stats = Player.transform.GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>();
+    }
+
+    private void setLvlPanels()
+    {
+        lvls = new GameObject[CountQuestions];
+        for (int i = 0; i < CountQuestions; i++)
+        {
+            lvls[i] = Instantiate(PlayerLvlTemplate, _scrollContent.transform);
+            lvls[i].name = "lvlPanel" + (i + 1);
+            lvls[i].transform.SetAsFirstSibling();
+            lvls[i].GetComponentInChildren<Text>().text = (i + 1).ToString();
+            if (i == 0)
+            {
+                lvls[0].transform.GetChild(0).gameObject.SetActive(true);
+                RectTransform num = lvls[0].transform.GetChild(1).GetComponent<RectTransform>();
+                num.offsetMin = new Vector2(60, num.offsetMin.y);
+            }
+        }
+        setLBContentHeight();
+    }
+    void setLBContentHeight()
+    {
+        //LeftTopBar
+        float scrollContentHeightLB = _scrollContent.transform.parent.GetComponent<RectTransform>().rect.height;
+        RectTransform rtLB = _scrollContent.GetComponent<RectTransform>();
+        rtLB.offsetMax = new Vector2(rtLB.offsetMax.x, Mathf.Abs(scrollContentHeightLB - (CountQuestions * 60)));
+    }
+
+
+    int loopCount = 0;
+    int makeRandomlyNumWithoutEquals(int[] targetNum, int min, int max)
+    {
+        int num;
+        do
+        {
+            num = UnityEngine.Random.Range(min, max);
+            loopCount++;
+            if(loopCount > 5000)
+            {
+                Debug.Log("Stop it");
+
+                break;
+            }
+        }
+        while (targetNum.Contains(num));
+
+        return num;
+    }
+    private int[] getRandomNumber(int min, int max, int count, bool equalNums)
+    {
+        int rand = UnityEngine.Random.Range(min, max);
+        int[] rands = new int[count];
+        for (int i = 0; i < rands.Length; i++)
+        {
+            rands[i] = -1;
+        }
+        for (int i = 0; i < rands.Length; i++)
+        {
+            while (!equalNums && rands.Contains(rand))
+            {
+                rand = UnityEngine.Random.Range(min, max);
+            }
+            if(equalNums)
+                rand = UnityEngine.Random.Range(min, max);
+
+            rands[i] = rand;
+        }
+
+        return rands;
+    }
+    ScaleEffect effect = new ScaleEffect();
+
+    bool isOnEvent = false;
+    public void OnEvent(EventData photonEvent)
+    {
+        if (photonEvent.Code == 52)
+        {
+            Debug.Log("Get Event");
+            WaitingPanel.SetActive(false);
+            isOnEvent = true;
+            Start();
+        }
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if (Server.isOwnerRoom)
+        {
+            Debug.Log("Send event");
+            WaitingPanel.SetActive(false);
+            RaiseEventOptions options = new RaiseEventOptions { Receivers = ReceiverGroup.Others };
+            SendOptions sendOption = new SendOptions { Reliability = true };
+            PhotonNetwork.RaiseEvent(52, "", options, sendOption);
+        }
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        WinOrLose(true);
+    }
+
+    public void WinOrLose(bool win)
+    {
+        WinPanel.SetActive(true);
+        if (win)
+        {
+            TextWinPanel.text = "Вы выграли!";
+        }
+        else
+        {
+            TextWinPanel.text = "Вы проиграли!";
+            ImageWinPanel.transform.localScale = new Vector3(.8f, .8f, 1);
+            ImageWinPanel.GetComponent<Image>().overrideSprite = Resources.Load<Sprite>("MathGame/FirstBattle/Abort");
+        }
+        StartCoroutine(effect.Scale(WinPanel.transform, new Vector3(1, 1, 1), 8f));
+        Time.timeScale = 0;
+    }
+    
+}
